1726d95139a09412504892cea4f73f97e6f7514a
At-work-Marcus <112435630+At-work-Marcus@users.noreply.github.com>
2022-08-29 17:27:20 -0400

    Update streamlit_app.py
    egg and coffee emojie added to first header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as sl
 # create title
 sl.title('My Parents Healthy Diner')
 # create header and sub text
-sl.header('Breakfast favorites')
+sl.header('🥚 Breakfast favorites ☕')
 sl.text('🥛 smoothie')
 sl.text('🥙 pancakes')
 sl.text('🍞 hard boiled egg')
