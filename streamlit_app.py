4121ab43d1ebac4f7b17f78b351c1cf23428ee33
At-work-Marcus <112435630+At-work-Marcus@users.noreply.github.com>
2022-09-21 11:05:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,5 @@ sl.dataframe(fruits_to_show)
 
 # lesson 9 Snowflake DABW
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-sl.text(fruityvice_response.json())
-
 sl.header("Fruityvice Fruit Advice!")
+sl.text(fruityvice_response.json())
